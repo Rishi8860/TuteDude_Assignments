9af69fef1b7c26c284f4ed587ae6b669e8ca4ac9
root <root@DESKTOP-NI5DJ8P.localdomain>
2025-09-07 14:19:41 +0530

    Assignment

---
+++ assignment_2/question_2.py
@@ -0,0 +1,9 @@
+students = {}
+
+students["Ram"]=88
+students["Shyam"]=85
+
+print(students)
+
+students["Ram"] = 97
+print(students)
---
+++ assignment_2/question_3.py
@@ -0,0 +1,4 @@
+with open("test.txt","w+") as file:
+    file.write("This is a dummy content")
+
+    
